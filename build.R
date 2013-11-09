4fbcc47d1987c39c8879f3b5275bebabcfbfca66
ellisp <peter.ellis2013nz@gmail.com>
2013-11-10 12:07:26 +1300

    minor fix to statplanet helpfile alias

--- build.R
+++ build.R
@@ -2,6 +2,8 @@ require(devtools)
 
 setwd("pkg")
 
+# PE laptop environemnt only
+# setwd("C:/Users/Peter Ellis/Documents/GitHub/mbie/pkg")
 
 unlink( '../output', TRUE)
 dir.create("../output", showWarnings=FALSE)
@@ -11,3 +13,4 @@ build(path= "../output", binary=TRUE)
 # next lines specific to Peter's home environment
 detach("package:mbie", unload=TRUE)
 install.packages("../output/mbie_0.6.0.zip", repos=NULL, type="win.binary")
+library(mbie)
