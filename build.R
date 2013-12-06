47218ff8030824330f6d30f3053baa1120e6dae5
ellisp <peter.ellis2013nz@gmail.com>
2013-12-07 11:14:51 +1300

    small improvements to helpfiles including to account for treemap upgrade

--- build.R
+++ build.R
@@ -2,11 +2,8 @@ require(devtools)
 
 setwd("pkg")
 
-# PE laptop environemnt only
-# setwd("C:/Users/Peter Ellis/Documents/GitHub/mbie/pkg")
+# PE laptop environment only
+build(path= "C:/Users/Peter Ellis/Dropbox/Packages")
+build(path= "C:/Users/Peter Ellis/Dropbox/Packages", binary=TRUE)
 
-unlink( '../output', TRUE)
-dir.create("../output", showWarnings=FALSE)
-build(path= "../output")
-build(path= "../output", binary=TRUE)
 
