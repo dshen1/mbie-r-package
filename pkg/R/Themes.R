ba5a6cb1d40553c7196fad130c2ff154846b30e7
ellisp <peter.ellis2013nz@gmail.com>
2014-01-10 08:00:23 +1300

    Added theme_nothing

---
+++ pkg/R/Themes.R
@@ -0,0 +1,13 @@
+# This file contains functions relating to ggplot2 themes for mbie
+
+theme_nothing <- function(base_size = 12, base_family = "Helvetica") {
+  theme_bw(base_size = base_size, base_family = base_family) %+replace%
+    theme(
+      rect = element_blank(),
+      line = element_blank(),
+      axis.ticks.length = unit(0.01, "cm"),
+      axis.ticks.margin = unit(0, "lines"),
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title = element_blank())
+}
