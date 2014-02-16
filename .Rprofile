5c6ac92a937d98bd643e8559c8fe16827dbe1d0e
Peter Ellis <peter.ellis@mbie.govt.nz>
2014-02-17 10:30:17 +1300

    Add .Rprofile, and fix build.R so it works in the MBIE environment.

---
+++ .Rprofile
@@ -0,0 +1,18 @@
+ 
+ .libPaths(c(.libPaths(), "P:/R/libraries/3.0.1"))
+
+ library(utils)
+ library(Defaults)
+ library(extrafont)
+ # library(mbie, lib.loc="P:/r/libraries/aaa test libraries")
+ library(mbie)
+ library(mbiedata)
+ theme_set( theme_grey( base_family= "Georgia"))
+ setDefaults(q, save="no")
+ useDefaults(q)
+ Sys.setenv(PDFLATEX="xelatex")
+ setInternet2(TRUE) 
+ Sys.setenv(R_GSCMD = '"C:/Program Files (x86)/gs/gs8.54/bin/gswin32c.exe"')
+ Sys.setenv(SDMX_CONF = "P:/R/configuration.properties")
+
+
--- build.R
+++ build.R
@@ -1,9 +1,12 @@
+
 require(devtools)
 
+# Set the library path so the Test folder is listed first and the installation will be to there,
+# rather than to the folder used for libraries by everyone
+.libPaths(c("P:/r/libraries/AAA TEST LIBRARIES", .libPaths()))
+
+
+# Build the package:
 setwd("pkg")
-
-# PE laptop environment only
-build(path= "C:/Users/Peter Ellis/Dropbox/Packages")
-build(path= "C:/Users/Peter Ellis/Dropbox/Packages", binary=TRUE)
-
+  build(path= "P:/OTSP/data-infrastructure/testing/", binary=TRUE)
 setwd("..")
