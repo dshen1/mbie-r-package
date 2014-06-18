e7ddf7dcb8c4a11a730aa8f98fdf117dea76deb0
Antonia Milkop <milkopa@DH-AUD05008JQ.wd.govt.nz>
2014-06-18 16:47:21 +1200

    updating mbie package with IVS2_location_17062014 data

--- .Rprofile
+++ .Rprofile
@@ -1,10 +1,16 @@
- 
- # the usual command to point the libraries at the right spot
- .libPaths(c(.libPaths(), "P:/R/libraries/3.0.1"))
+### 	This .rprofile has the TEST folder listed first and THEN the usual command folder
+##		When you load a package it looks in each library location in order and the first time it finds
+##		the sought after package it loads it.  
+##		For the purposes of testing the new build we want R to load the TEST libraries first
+
 
  # Set the library path so the Test folder is listed first and the installation will be to there,
 # rather than to the folder used for libraries by everyone
 .libPaths(c("P:/r/libraries/AAA TEST LIBRARIES", .libPaths()))
+ 
+ # the usual command to point the libraries at the right spot
+ .libPaths(c(.libPaths(), "P:/R/libraries/3.0.1"))
+
 
 
  library(utils)
--- grooming/Importing IVS2 locations into locations file.r
+++ grooming/Importing IVS2 locations into locations file.r
@@ -1,28 +1,28 @@
 ##  Purpose:  Creates locations_old.rda and locations_2013.rda
 ##			in the past there was just the locations.rda.  We are replacing this with locations_old 
-##			and adding a new locations_2013.rda for the uptodate locations in IVS2
+##			and adding a new locations_2013.rda for the up to date locations in IVS2
 ##	Developed by: Antonia Milkop
 ##	Date: 27 May 2014
 ##	Peer reviewed:
 
-#one this script has been run once, this next line won't work because we have replaced locations with locations_old (i.e. deleted locations)
+#one this script has been run once, this next line won't work because we have replaced locations with locations_old (i.e. deleted locations) - see below
 data(locations)
 save(locations, file="raw_data/locations.rda")
 
 ##		add another field to identify whether data is from locations1 or locations2 (i.e. IVS1 or IVS2)
 ##  	currently locations has 2500 ish rows.  locations2 has another 300 odd.  So we'll have about 3000 rownames in the end
-
-#bring in the new 'locations2' list of IVS2 location names
 locations_old <- locations
-locations_2013 <- read.csv("raw_data/IVS2_location_26052014.csv")
-
 save(locations_old, file = "pkg/data/locations_old.rda")
+
+#bring in the new 'locations2' list of IVS2 location names, and save it in data folder 
+locations_2013 <- read.csv("raw_data/IVS2_location_17062014.csv")
+
 save(locations_2013, file = "pkg/data/locations_2013.rda")
 
 ## Next steps:
-## 	1. Delete the existing 'locations.rda' file in pkg/data
-##  2. run the build.R file , this will 'build' your new package with these new data files in
-##  3. Document what you've done in the help files
+## 1. Delete the existing 'locations.rda' file in pkg/data
+## 2. run the build.R file , this will 'build' your new package with these new data files in
+## 3. Document what you've done in the help files
 ##			pkg/man you'll need two .Rd files, locations_old.Rd and locations_2013.Rd and delete the old locations.Rd file
 ##			pkg/man/mbie-package.Rd
 ##			pkg/"DESCRIPTION"
