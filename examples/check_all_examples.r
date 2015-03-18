74fe4c497f6ebe365b64470a2d5e704d8c866bd2
Haizhen(Eric) Wu <Eric.Wu@mbie.govt.nz>
2015-03-18 16:03:59 +1300

    add script to check all examples.

--- examples/check_all_examples.r
+++ examples/check_all_examples.r
@@ -1,9 +1,13 @@
 library("mbie", lib.loc = "P:/R/libraries/3.1.2")
 
 tmp <- help.search("", package = "mbie", lib.loc = "P:/R/libraries/3.1.2")
-tmp$matches[,"name"]
-for(topic in tmp$matches[,"name"]){
+topics <- tmp$matches[,"name"]
+for(i in 15:length(topics)){
+  (topic <- topics[i])
   do.call(example, list(topic,package = "mbie", lib.loc = "P:/R/libraries/3.1.2"))
 }
 
 
+
+library("mbiedata", lib.loc = "P:/R/libraries/3.1.2")
+library("mbiemaps", lib.loc = "P:/R/libraries/3.1.2")
