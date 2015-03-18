805e6a4ee3d7059dc8cb58a5c5472bb06d3639cd
Haizhen(Eric) Wu <Eric.Wu@mbie.govt.nz>
2015-03-18 16:08:35 +1300

    fix bugs in check_all_example.r

--- examples/check_all_examples.r
+++ examples/check_all_examples.r
@@ -1,13 +1,18 @@
-library("mbie", lib.loc = "P:/R/libraries/3.1.2")
+target.lib.loc <- "P:/R/libraries/3.1.2"
 
-tmp <- help.search("", package = "mbie", lib.loc = "P:/R/libraries/3.1.2")
+install.packages("P:/OTSP/data-infrastructure/archive/mbie_0.8.9.tar.gz", lib = target.lib.loc, repos = NULL, type = "source")
+
+
+library("mbie", lib.loc = target.lib.loc)
+
+tmp <- help.search("", package = "mbie", lib.loc = target.lib.loc)
 topics <- tmp$matches[,"name"]
-for(i in 15:length(topics)){
+for(i in 25:length(topics)){
   (topic <- topics[i])
-  do.call(example, list(topic,package = "mbie", lib.loc = "P:/R/libraries/3.1.2"))
+  do.call(example, list(topic,package = "mbie", lib.loc = target.lib.loc))
 }
 
 
 
-library("mbiedata", lib.loc = "P:/R/libraries/3.1.2")
-library("mbiemaps", lib.loc = "P:/R/libraries/3.1.2")
+library("mbiedata", lib.loc = target.lib.loc)
+library("mbiemaps", lib.loc = target.lib.loc)
