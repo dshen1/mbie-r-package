c52f26d9bdf90f5971be54e5c070b938834f8d19
mcmillpe <pete.mcmillen@mbie.govt.nz>
2015-06-24 16:27:41 +1200

    push ongoing changes to rd, example and other files to replace plyr and reshape2 functionality with dplyr and tidyr

--- examples/animation_frames examples.R
+++ examples/animation_frames examples.R
@@ -4,22 +4,33 @@ plot1 <- ggplot(mtcars, aes(x=disp, y=mpg)) + geom_point() + geom_smooth(method=
 
    animation_frames(plot1, start = 2, dev="png", method = "by row")
 
+   
 # Example 2 - simple time series line plot
 data(AirPassengers)
 AirPassengers_df <- data.frame(AirPassengers)
 AirPassengers_df$Period <- time(AirPassengers)
-plot2 <- ggplot(AirPassengers_df, aes(x=Period, y=AirPassengers)) + geom_line() + xlim(1948, 1962) + ylim(0, 700)
-animation_frames(plot2, start = 10, dev="png", filename="AirPassengers")
+
+plot2 <- ggplot(AirPassengers_df, aes(x = Period, y = AirPassengers)) + geom_line() + xlim(1948, 1962) + ylim(0, 700)
+
+   animation_frames(plot2, start = 10, dev = "png", filename = "AirPassengers")
+
 
 # Example 3 - more complex time series
 library(Cairo)
+library(dplyr)
+library(mbiedata)
 data(IVStrips)
-tmp <- ddply(IVStrips, .(YEJun, POVTop3, COPRTop5), summarise, Spend = sum(FinalWeight * SmoothTotalSpend * Adjusted_Factor))
-plot3 <- ggplot(tmp, aes(x=YEJun, y = Spend / 10 ^ 6, colour=COPRTop5)) +
+
+tmp <- IVStrips %>%
+            group_by(YEJun, POVTop3, COPRTop5) %>%
+            summarise(Spend = sum(FinalWeight * SmoothTotalSpend * Adjusted_Factor))
+
+plot3 <- ggplot(tmp, aes(x = YEJun, y = Spend / 10 ^ 6, colour = COPRTop5)) +
 	facet_wrap( ~ POVTop3) +
 	geom_line() +
-	scale_colour_manual(values=tourism.cols()) +
-	scale_y_continuous("Total tourist spend ($m)\n", label=comma, limits=c(0, 1.7 * 10 ^ 3)) +
-	scale_x_continuous("", limits=c(1997, 2014)) +
-	theme_bw(base_family="Calibri")
-   animation_frames(plot3, filename="IVS", width=9, height=7)
+	scale_colour_manual(values = tourism.cols()) +
+	scale_y_continuous("Total tourist spend ($m)\n", label = comma, limits = c(0, 1.7 * 10 ^ 3)) +
+	scale_x_continuous("", limits = c(1997, 2014)) +
+	theme_bw(base_family = "Calibri")
+   
+   animation_frames(plot3, filename = "IVS", width = 9, height = 7)
--- examples/forecasts examples.R
+++ examples/forecasts examples.R
@@ -1,16 +1,17 @@
 data(forecasts)
 
-final <- subset(forecasts, Year==2019)[, c("Country", "Total.Visitor.Spend")]
+final <- subset(forecasts, Year == 2019)[, c("Country", "TotalVisitorSpend")]
 
-forecasts$Country <- factor(forecasts$Country, levels=final$Country[order(-final$Total.Visitor.Spend)])
+forecasts$Country <- factor(forecasts$Country, levels = final$Country[order(-final$TotalVisitorSpend)])
 
-ggplot(forecasts, aes(x=Year, color=Country, linetype=Type, y=Total.Visitor.Spend / 10^6)) +
-  geom_line(size=2) +
-  labs(x="", y="Total visitor spend ($m)\n", title="Tourism forecasts, New Zealand") +
-  scale_color_manual(values=tourism.cols("Alternating"))
+ggplot(subset(forecasts, Year >= 1998), aes(x = Year, y = TotalVisitorSpend, colour = Country, linetype = Type)) +
+   geom_line(size = 1) +
+   scale_y_continuous(label = dollar) +
+   labs(x = "", y = "Total visitor spend ($m)\n", title = "Tourism forecasts, New Zealand") +
+   scale_color_manual(values = tourism.cols("Alternating"))
 
 
-ggplot(forecasts, aes(x=Total.Visitor.Spend / 10^6, y=Total.Visitor.Days / 10^6, color=Year)) +
-  geom_text(aes(label=Year), size=3) +
-  geom_path() +
-  facet_wrap(~Country, scale="free")
+ggplot(subset(forecasts, Year >= 1998), aes(x = TotalVisitorSpend, y = TotalVisitorDays / 10^6, colour = Year)) +
+   geom_text(aes(label = Year), size = 3) +
+   geom_path() +
+   facet_wrap(~Country, scale = "free")
