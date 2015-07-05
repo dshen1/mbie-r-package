7244ab30a0d7717c7aa5f5b51b8fdd8d83d6c075
mcmillpe <pete.mcmillen@mbie.govt.nz>
2015-07-06 09:56:51 +1200

    push cosmetic change to build script

--- build.R
+++ build.R
@@ -5,6 +5,7 @@ require(devtools)
 # Note - set the library path so the Test folder is listed first and the installation will be to there,
 # rather than to the folder used for libraries by everyone.  This should be done in the .Rprofile
 # but in case not you can always run the line below:
+
 .libPaths(c("P:/R/libraries/AAA TEST LIBRARIES",.libPaths()))
 ############################################################################
 
